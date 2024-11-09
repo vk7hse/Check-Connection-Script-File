35ee9d1830c5c7a36462501e3a4b943631cc8bbf
Freddie Mac - KD5FMU <fred.mcguire.jr@gmail.com>
2024-11-09 07:18:35 -0600

    check_connetion.sh
    Check Connection Script Upload

---
+++ check_connection.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check Connection to a designated AllStarLink node and reconnect if disconnected
+
+# Set your AllStarLink node number and the target node number you want to check to the connection
+MY_NODE=XXXXXX
+TARGET_NODE=XXXXXX
+
+# Check if the target node is connected
+if ! /usr/sbin/asterisk -rx "rpt lstats $MY_NODE" | grep -q "$TARGET_NODE"; then
+  echo "Node $MY_NODE is not connected to node $TARGET_NODE. Reconnecting..."
+  /usr/sbin/asterisk -rx "rpt fun $MY_NODE *3$TARGET_NODE"
+else
+   echo "Node $MY_NODE is already connected to node $TARGET_NODE."
+ fi
