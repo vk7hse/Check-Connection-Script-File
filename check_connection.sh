88f5151027695013d5b6f612f36795ca597c6b0a
Rob - K6IRK <trebor57@users.noreply.github.com>
2024-11-17 15:37:33 -0800

    Update check_connection.sh
    Added for more than 1 node to make sure is connected

--- check_connection.sh
+++ check_connection.sh
@@ -13,3 +13,15 @@ if ! /usr/sbin/asterisk -rx "rpt lstats $MY_NODE" | grep -q "$TARGET_NODE"; then
 else
    echo "Node $MY_NODE is already connected to node $TARGET_NODE."
  fi
+
+# comment out all the below if not adding a second section, otherwise fill out MY_NODE and TARGET_NODE
+ MY_NODE=XXXXXX
+TARGET_NODE=XXXXXX
+
+# Check if the target node is connected
+if ! /usr/sbin/asterisk -rx "rpt lstats $MY_NODE" | grep -q "$TARGET_NODE"; then
+  echo "Node $MY_NODE is not connected to node $TARGET_NODE. Reconnecting..."
+  /usr/sbin/asterisk -rx "rpt fun $MY_NODE *3$TARGET_NODE"
+else
+   echo "Node $MY_NODE is already connected to node $TARGET_NODE."
+ fi
